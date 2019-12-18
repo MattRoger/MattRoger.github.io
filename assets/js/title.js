fa65ec6a45e95d37c79462ee077881f6c1418158
MattRoger <mrogerphotography@gmail.com>
2019-12-18 14:23:42 -0700

    intro is up!

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,5 +1,38 @@
 window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
-
-  $(".opening").delay(1000).text("To seek something is to believe in its possibility.")
-});
+  openMessage();
+  setTimeout(
+    function logoMessage(){
+      const logo= $("<img>");
+      logo.attr("src", "./assets/img/Star_Wars_logo.png");
+      $(".logo").append(logo)     
+      console.log("out")  
+    }
+    ,4500
+    )
+   setTimeout(
+     function crawl(){     
+       const pt=$("<p>").text("Episode III")
+       const ph=$("<h1>").text("A New Beginning")
+       $(".title").append(pt, ph)
+       const p1=$("<p>").text("It is a period of peace. Rebel team leader and recent University of Denver full-stack coding Bootcamp, Matt Roger, looks for new opportunities. At his current position, he launches highly effective stikes with mass email marketing")
+       const p2=$("<p>").text("Trained in the art of Full-stack development and strong with the creative side of the force, this rebel coder is looking for a new company to grow with and fight the galactic empire of mediocre web design")
+       const p3=$("<p>").text(" Pursued by the Empire’s sinister agents, Matt races home aboard his starship, custodian of the plans that can save his people and restore creativity to the galaxy…")
+      $(".crawl").append(p1, p2, p3)    
+      },1000
+   )
+   setTimeout(
+     function enterButton(){
+      const enterBtn=$("<a>").text("Enter Site")
+      enterBtn.attr("href","about.html")
+      $(".enterSite").append(enterBtn)
+     }
+   ), 10000    
+  });
+  
+  function openMessage (){
+    const openMessage=$("<p>")
+    openMessage.text("To seek something is to believe in its possibility.");
+    $(".opening").append(openMessage)
+    $(".opening").fadeOut(3000)
+  }
